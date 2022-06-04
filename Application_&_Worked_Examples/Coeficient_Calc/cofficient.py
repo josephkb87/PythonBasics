d093da7ba05782277623c6af9bc6e2b982bc6642
JungBasher87 <73903994+josephkb87@users.noreply.github.com>
2022-06-04 18:21:06 +0300

    Create cofficient.py

---
+++ Application_&_Worked_Examples/Coeficient_Calc/cofficient.py
@@ -0,0 +1,14 @@
+# Finding the ratio of two sin angles#
+import math
+a = input("Please enter the value of a:", int())
+b = input("Enter the value of b", int())
+
+print("The sine of a is:", math.sin(a))
+print("The cosine of b is:", math.cos(b))
+
+
+
+
+# The ratio of the sine to sine
+print( "THE RATIO OF sin a to sinb is",(math.sin(a))/(math.cos(b)))
+
