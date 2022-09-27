5ce265fdcdec0daf2aadd4479b1972933d4587fa
JungBasher87 <73903994+josephkb87@users.noreply.github.com>
2022-09-27 18:06:06 +0300

    Github actions workflow for profile update.

--- Application And Worked Examples/Coeficient_Calc/cofficient.py
+++ Application And Worked Examples/Coeficient_Calc/cofficient.py
@@ -6,9 +6,6 @@ b = input("Enter the value of b", int())
 print("The sine of a is:", math.sin(a))
 print("The cosine of b is:", math.cos(b))
 
-
-
-
 # The ratio of the sine to sine
 print( "THE RATIO OF sin a to sinb is",(math.sin(a))/(math.cos(b)))
 
